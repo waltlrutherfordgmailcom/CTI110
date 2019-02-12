36cd41eb029a9a418cf13deda81d370cdb88dbad
waltlrutherfordgmailcom <47375051+waltlrutherfordgmailcom@users.noreply.github.com>
2019-02-12 13:31:32 -0500

    Sales prediction program

---
+++ P2T1_SalesPrediction_WalterRutherford.py
@@ -0,0 +1,25 @@
+# Sales prediction exercise 2 Chapter 2. Calculating an annual proffit.
+# 02/05/2019
+# CTI-110 P2T1 - Sales Prediction
+# Walter Rutherford
+
+total_sales = float( input("Enter the projected amount of total sales:"))
+
+annual_profit = .23 * total_sales
+
+print ("Your annual profit is $", annual_profit)
+    
+
+
+
+
+
+
+
+
+
+
+         
+
+        
+    
