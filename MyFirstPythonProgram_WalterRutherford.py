9fb4a39171768bb1c80fe245b7d4c3daa34bbf60
waltlrutherfordgmailcom <47375051+waltlrutherfordgmailcom@users.noreply.github.com>
2019-02-12 13:34:35 -0500

    My first Python program

---
+++ MyFirstPythonProgram_WalterRutherford.py
@@ -0,0 +1,6 @@
+# My First Python Program (Exercise number 2): An exercise in which explains how to interact with IDLE.
+# 01/24/2019
+# CTI-110 Tutorial 1 - My First Python Program
+# Walter Rutherford
+#
+print ('This is a test of IDLE')
