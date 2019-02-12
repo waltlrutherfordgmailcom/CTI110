c8b5fa4e77afd4cfa4415dbe5b8c06830fcf4f62
waltlrutherfordgmailcom <47375051+waltlrutherfordgmailcom@users.noreply.github.com>
2019-02-12 13:33:07 -0500

    Program for converting pounds to kilograms

---
+++ P2HW1_PoundsKilograms_WalterRutherford.py
@@ -0,0 +1,26 @@
+# Description: Writing a program that converts pounds to kilograms; The
+# original formula used was kg=lb/2.2046 
+# Date: 2/6/2019
+# Course: CTI-110 P2HW1 - Pounds to Kilograms Converter
+# Walter, Rutherford
+
+# Get the amount of pounds
+# Build a variable for 2.2046 as the expression.
+# Calculate the amont of Kilograms using an equation comprised of both pounds,
+# and any chosen variable for the decimal value.
+# Display the amount of kilograms.
+
+# 1.Create input for pounds
+Pounds = int(input ('How many pounds? '))
+
+# 2.Assign variable x variable for 2.2046 espression.
+x = 2.2046
+
+# 3.creat equation for kilograms 
+Kilograms= Pounds / x
+
+# 4.Display reponse for kilograms.
+print("Kilograms:", Kilograms, "Kgs")
+
+
+
