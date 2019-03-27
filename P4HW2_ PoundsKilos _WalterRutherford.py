f292d1a882580a5d16d65749ef7f34fe61fd96e2
waltlrutherfordgmailcom <47375051+waltlrutherfordgmailcom@users.noreply.github.com>
2019-03-27 10:18:33 -0400

    Pounds to Kilograms 2

--- P4HW2_ PoundsKilos _WalterRutherford.py
+++ P4HW2_ PoundsKilos _WalterRutherford.py
@@ -1,34 +1,33 @@
 # This program displays a table of lbs to kgs, with a step value of 10. 
-# 2/26/2019
-# CTI-110 P4HW2 - Pounds to Kilos Table
+# 3/14/2019
+# CTI-110 P4HW2 - Pounds to Kilos Table.
 # Rutherford, Walter
 
-
-# Create function
-# Display labels
-# Creat for loop
-# Disolay pounds and kilograms
-
+# Create function.
+# Display labels.
+# Creat for loop.
+# Disolay pounds and kilograms.
 
 
 
-# Beginning of function
+
+# Beginning of Function.
 def Pounds_Conversian():
 
-    # Print labels
+    # Displayed Labels.
     print ('Pounds\t Kilograms')
     print('_____________________')
 
     # For loop 100 to 300 in incraments of 10. 
     for Pounds in range(100,301,10):
 
-                # Variable equation for kilograms
+                # Variable equation for kilograms.
                 Kilograms = Pounds / 2.2046
 
-                # Printed results
+                # Printed results.
                 print('\n',Pounds,'\t',format(Kilograms,'.4f'))
 
-# End of function
+# End of function.
 Pounds_Conversian()
         
             
