5e512c8711694fc37afa312bf8901a97dec1b824
waltlrutherfordgmailcom <47375051+waltlrutherfordgmailcom@users.noreply.github.com>
2019-04-04 17:24:19 -0700

    kilometer converter with functions.

---
+++ P5T1_KilometerConverter_WalterRutherford.py
@@ -0,0 +1,28 @@
+# This program converts Kilometers to miles.
+# 3/28/2019
+# CTI-110 P5T1_KilometerConverter 
+# Walter Rutherford
+#
+
+# Get kilometers.
+# Create simulation.
+# Display results.
+
+
+# Defining of function.
+def kilo_converter():
+
+
+    # Input asking for distance in kilograms.
+    Kilometers= int(input( "Enter the distance in kilometers:  "))
+    # Variable equation for miles.
+    Miles = Kilometers * 0.6214
+    # Print statement with the result of miles.
+    print(Kilometers,"kilometers is equal to",Miles,"miles")
+
+
+
+
+
+# Call to function.
+kilo_converter()
